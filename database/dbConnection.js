24ffa7f826a0da53e859baccd2e071786d9e8ad0
daniel <danielsprado12@gmail.com>
2020-12-10 14:58:19 -0300

    teste

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -1,3 +1,5 @@
 module.exports =  {
-    url: "mongodb://localhost:27017/topicos3",
+    url: "mongodb+srv://topicos3:daniel-123@cluster0.2dqxj.mongodb.net/test?retryWrites=true&w=majority",
 }
+
+//mongodb+srv://topicos3:<password>@cluster0.2dqxj.mongodb.net/test
