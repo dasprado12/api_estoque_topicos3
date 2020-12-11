37de84dc9a147b993500e91445965e3a7a639e83
MirellaSales <mi.rella_sales@hotmail.com>
2020-12-11 09:50:38 -0300

    New Frontzin

--- config/auth.js
+++
@@ -1,4 +0,0 @@
-export default {
-    secret: "72ed0aaf24fb574bdf9218150b9e1b69",
-    expiresIn: "1d"
-  };
--- src/server.js
+++
@@ -1,6 +0,0 @@
-const app = require("./app")
-const bodyParser = require("body-parser")
-
-app.use(bodyParser.json())
-
-app.listen(3000);
